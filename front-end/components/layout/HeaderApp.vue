09bd5d88355ad08c70fa462df2ca5b03d0c0e49c
hita.dev.duoc <duoc.bedev@hitak.net>
2022-06-10 15:38:16 +0700

    update UI

--- front-end/components/layout/HeaderApp.vue
+++ front-end/components/layout/HeaderApp.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="header shadow">
-    <b-navbar toggleable="lg" type="light" variant="light" fixed="top">
+    <b-navbar toggleable="lg" type="light" variant="white" fixed="top">
       <!-- <b-container fluid> -->
       <b-navbar-brand href="#">
         <div class="font-weight-bold">Nhà Thuê Cần Thơ</div>
